2699ef4722ef086160a5b6841dc9a41e54815a99
RVStank <stankov.rostislav@gmail.com>
2024-11-02 15:31:08 +0000

    O(n) time

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from typing import List
+from typing import List, Optional
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        moves = 0
+        x1, y1 = points.pop()
+        while points:
+            x2, y2 = points.pop()
+            moves += max(abs(y2 - y1), abs(x2 - x1))
+            x1, y1 = x2, y2
 
-        
+        return moves
+
+
 if __name__ == "__main__":
     solution = Solution()
+    solution.minTimeToVisitAllPoints([[1,1], [3,4], [-1,0]])
